f6de5394c2d112773abd7c408e335153ce43f313
izabelaracz <izabelaraczynskaa@gmail.com>
2020-12-13 16:02:49 +0100

    Dodano ikonę aplikacji, uzupełniono aktywność dom, poprawiono grafikę kropelki wody RP

---
+++ app/src/main/java/com/example/myekoapplication/dom_domowe_srodki.java
@@ -0,0 +1,14 @@
+package com.example.myekoapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dom_domowe_srodki extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dom_domowe_srodki);
+    }
+}
